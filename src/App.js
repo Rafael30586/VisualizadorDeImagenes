7ed2045497f58d755b771f86c7346716d512f0c0
F. Rafael Alvarez <fraq86@gmail.com>
2023-10-03 10:09:06 -0300

    correccion de errores de sintaxis

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Tarjeta from './components/Tarjeta';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Tarjeta></Tarjeta>
     </div>
   );
 }
--- src/components/Tarjeta.js
+++ src/components/Tarjeta.js
@@ -2,33 +2,35 @@ import React, { useEffect, useState } from 'react'
 
 const Tarjeta = () => {
 
-    const [url, setUrl] = useState["https://www.dzoom.org.es/wp-content/uploads/2017/07/seebensee-2384369-810x540.jpg"];
-    const [ancho, setAncho] = useState[18];
-    const [propiedad, setPropiedad] = useState["brightness"];
-    const [valor, setValor] = useState[100];
-    const [input, setInput] = useState[{}];
-    const [imagen,setImagen] = useSate[{}];
+    const [url, setUrl] = useState("https://www.dzoom.org.es/wp-content/uploads/2017/07/seebensee-2384369-810x540.jpg");
+    console.log(url);
+    const [ancho, setAncho] = useState(18);
+    const [propiedad, setPropiedad] = useState("brightness");
+    const [valor, setValor] = useState(100);
+    const [input, setInput] = useState({});
+    const [imagen,setImagen] = useState({});
 
     useEffect(()=>{
-        setPropiedad(100);
+        setPropiedad("brightness");
+        setValor(100);
         setAncho(18);
         setInput(document.getElementById("inputFoto"));
         setImagen(document.getElementById("imagen"));
     },[url]);
 
   return (
-      <div>
-          <div class="card" style={{width: {ancho} + 'em'}}>
+      <div class="container mt-4" id="contenedor">
+          <div class="card" style={{width: ancho + 'em'}}>
               <img src={url} class="card-img-top" id="imagen" alt="Foto" style={{filter: `${propiedad}(${valor}%)`}}></img>
               <div class="card-body">
-                  <input type='text' id="inputFoto"></input>
+                  <input type='text' id="inputFoto" class="mx-auto"></input>
                   <h5 class="card-title">Card title</h5>
                   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <button type="button" class="btn btn-success" onClick={()=>setUrl(input.value)}>Success</button>
-                  <button type="button" class="btn btn-danger">Danger</button>
+                  <button type="button" class="btn btn-success" onClick={()=>setUrl(input.value)}>Cargar imagen</button>
+                  <button type="button" class="btn btn-danger">Tema</button>
               </div>
           </div>
-          <div id="botones">
+          <div id="botones" class="mx-auto">
               <div class="btn-group" role="group" aria-label="Basic example">
                   <button type="button" class="btn btn-primary" onClick={()=>setAncho(ancho+5)}>Agrandar</button>
                   <button type="button" class="btn btn-primary" onClick={()=>setAncho(ancho-5)}>Achicar</button>
