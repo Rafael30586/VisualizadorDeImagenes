7b0ccfcb791d4709bf8c55d7f56772608b621209
F. Rafael Alvarez <fraq86@gmail.com>
2023-10-04 09:12:08 -0300

    Funciones para eventos con los hooks

--- src/components/Tarjeta.js
+++ src/components/Tarjeta.js
@@ -9,9 +9,45 @@ const Tarjeta = () => {
     const [valor, setValor] = useState(100);
     const [input, setInput] = useState({});
     const [imagen,setImagen] = useState({});
+    const [propiedadTitulo, setPropiedadTitulo] = useState("Brillo: ");
+
+    function agrandar(){
+        setAncho(ancho+5);
+    }
+
+    function achicar(){
+        setAncho(ancho-5);
+    }
+
+    function modificarBrillo(){
+        setPropiedad("brightness");
+        setPropiedadTitulo("Brillo: ");
+        setValor(100);
+    }
+
+    function modificarGrises(){
+        setPropiedad("grayscale");
+        setPropiedadTitulo("Grises: ");
+        setValor(75);
+    }
+
+    function modificarSepia(){
+        setPropiedad("sepia");
+        setPropiedadTitulo("Sepia: ");
+        setValor(75);
+    }
+
+    function aumentar(){
+        setValor(valor+5);
+    }
+
+    function disminuir(){
+        setValor(valor-5);
+    }
 
     useEffect(()=>{
         setPropiedad("brightness");
+        setPropiedadTitulo("Brillo: ");
         setValor(100);
         setAncho(18);
         setInput(document.getElementById("inputHeader"));
@@ -20,17 +56,17 @@ const Tarjeta = () => {
 
   return (
       <div className="container mt-1" id="contenedor">
-          <header className="p-3 text-bg-dark">
+          <header className="p-3 text-bg-dark sticky-top">
               <div className="container">
                   <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                       <div className="text-strat">
-                          <button type="button" className="btn btn-outline-light me-2" onClick={()=>setAncho(ancho+5)}>Agrandar</button>
-                          <button type="button" className="btn btn-outline-light me-2" onClick={()=>setAncho(ancho-5)}>Achicar</button>
-                          <button type="button" className="btn btn-info me-2" onClick={()=>setPropiedad("brightness")}>Brillo</button>
-                          <button type="button" className="btn btn-info me-2" onClick={()=>setPropiedad("grayscale")}>Grises</button>
-                          <button type="button" className="btn btn-info me-2" onClick={()=>setPropiedad("sepia")}>Sepia</button>
-                          <button type="button" className="btn btn-warning me-2" onClick={()=>setValor(valor+5)}>+</button>
-                          <button type="button" className="btn btn-warning me-2" onClick={()=>setValor(valor-5)}>-</button>
+                          <button type="button" className="btn btn-info me-2" onClick={agrandar}>Agrandar</button>
+                          <button type="button" className="btn btn-info me-2" onClick={achicar}>Achicar</button>
+                          <button type="button" className="btn btn-warning me-2" onClick={modificarBrillo}>Brillo</button>
+                          <button type="button" className="btn btn-warning me-2" onClick={modificarGrises}>Grises</button>
+                          <button type="button" className="btn btn-warning me-2" onClick={modificarSepia}>Sepia</button>
+                          <button type="button" className="btn btn-warning me-2" onClick={aumentar}>+</button>
+                          <button type="button" className="btn btn-warning me-2" onClick={disminuir}>-</button>
                       </div>
                       <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                           
@@ -41,8 +77,8 @@ const Tarjeta = () => {
                       </form>
 
                       <div className="text-end">
-                          <button type="button" className="btn btn-outline-light me-2" onClick={() => setUrl(input.value)}>Cargar imagen</button>
-                          <button type="button" className="btn btn-outline-warning">Tema</button>
+                          <button type="button" className="btn btn-success me-2" onClick={() => setUrl(input.value)}>Cargar imagen</button>
+                          <button type="button" className="btn btn-light">Tema</button>
                       </div>
                   </div>
               </div>
@@ -50,8 +86,7 @@ const Tarjeta = () => {
           <div className="card p-1" style={{width: ancho + 'em'}}>
               <img src={url} className="card-img-top" id="imagen" alt="Foto" style={{filter: `${propiedad}(${valor}%)`}}></img>
               <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <h5 className="card-title">{propiedadTitulo}{valor}</h5>
               </div>
           </div>
       </div>
